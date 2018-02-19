2908e32701f59033f322851b2ea0f44401706496
kek12e <katiekyle93@gmail.com>
2018-02-19 16:34:29 -0500

    antiSMASH_annotation.sh optional args
    add command line options to antiSMASH_annotations.sh
    -usage()
    -argument parsing loops

--- ClustCompare/src/antiSMASH_annotation.sh
+++ ClustCompare/src/antiSMASH_annotation.sh
@@ -4,71 +4,117 @@
 # Will put annotations in ../Data/ and modify ../CHANGELOG.txt
 # v3.0 January 2, 2018 - adapted to Jan 2018 script versions. Requires all genomes be located in ../Data/genomes. Runs on .gbk, .gbff, .fasta, .fna, .fa nucleotide formats.
 # v3.1 February 15, 2018 - Accommodates genomes.list output file
+# v3.2 February 19, 2018 - adds command line options for number of threads and data paths
 
-# Add note to CHANGELOG.txt
-echo -e "## `date +%Y-%m-%d:%H:%M:%S` \n\n* antiSMASH_annotations.sh v3.0 started" >> ../CHANGELOG.txt 
+usage()
+{
+	echo "USAGE:"
+	echo "Syntax: ./antiSMASH_annotation.sh [OPTIONS]"
+	echo "Options:"
+	echo "    -c    number of CPU threads"
+	echo "    -i    path to directory containing input data files"
+	echo "    -o    path to directory where output should be placed"
+	echo "    -l    path to log file to update"
+	echo "    -h    print this help message"
+}
 
-# check for ../Data/genomes directory
-if [ -d "../Data/genomes/" ]; then
-	echo "../Data/genomes found"
+# default parameters
+NPROC=$(nproc)
+INPUT_DATA_PATH=../Data/genomes
+OUTPUT_DIR=../Data/antiSMASH_annotations
+LOG_FILE=${OUTPUT_DIR}/CHANGELOG.txt
+
+# parse command line options
+while [ "$1" != "" ]; do
+	case $1 in
+		-c )	shift
+			NPROC=$1
+			;;
+		-i )	shift
+			INPUT_DATA_PATH=$1
+			;;
+		-o )	shift
+			OUTPUT_DIR=$1
+			;;
+		-l )	shift
+			LOG_FILE=$1
+			;;
+		-h )	usage
+			exit
+			;;
+		* )	usage
+			exit 1
+	esac
+	shift
+done
+
+echo "Using $NPROC cores"
+echo "Looking for input data here: $INPUT_DATA_PATH"
+echo "Ouput will be here: $OUTPUT_DIR"
+echo "Updating log: $LOG_FILE"
+
+# Add note to log
+echo -e "## `date +%Y-%m-%d:%H:%M:%S` \n\n* antiSMASH_annotations.sh v3.0 started" >> $LOG_FILE 
+
+
+# check for $INPUT_DATA_PATH directory
+if [ -d $INPUT_DATA_PATH ]; then
+	echo "$INPUT_DATA_PATH found"
 else
-	echo "../Data/genomes not found - exiting antiSMASH_annotation.sh"
+	echo "$INPUT_DATA_PATH not found - exiting antiSMASH_annotation.sh"
 	exit
 fi
 
-# find number of cores
-NPROC=$(nproc)
-echo "Using $NPROC cores"
 
 # unzip any zipped files, if they exist
-for f in ../Data/genomes/*.gbff.gz; do
+for f in ${$INPUT_DATA_PATH}/*.gbff.gz; do
 	[ -e "$f" ] && gunzip $f && echo "unzipping $f"
 done
 
-for f in ../Data/genomes/*.gbk.gz; do
+for f in ${$INPUT_DATA_PATH}/*.gbk.gz; do
 	[ -e "$f" ] && gunzip $f && echo "unzipping $f"
 done
 
-for f in ../Data/genomes/*.embl.gz; do
+for f in ${$INPUT_DATA_PATH}/*.embl.gz; do
 	[ -e "$f" ] && gunzip $f && echo "unzipping $f"
 done
 
-for f in ../Data/genomes/*.fasta.gz; do
+for f in ${$INPUT_DATA_PATH}/*.fasta.gz; do
 	[ -e "$f" ] && gunzip $f && echo "unzipping $f"
 done
 
-for f in ../Data/genomes/*.fna.gz; do
+for f in ${$INPUT_DATA_PATH}/*.fna.gz; do
 	[ -e "$f" ] && gunzip $f && echo "unzipping $f"
 done
 
-for f in ../Data/genomes/*.fa.gz; do
+for f in ${$INPUT_DATA_PATH}/*.fa.gz; do
 	[ -e "$f" ] && gunzip $f && echo "unzipping $f"
 done
 
 
 # create input file list
 
-for f in ../Data/genomes/*.gbff; do
+for f in ${$INPUT_DATA_PATH}/*.gbff; do
 	[ -e "$f" ] && ls $f >> genomes.list
 done
 
-for f in ../Data/genomes/*.gbk; do
+for f in ${$INPUT_DATA_PATH}/*.gbk; do
 	[ -e "$f" ] && ls $f >> genomes.list
 done
 
-for f in ../Data/genomes/*.embl; do
+for f in ${$INPUT_DATA_PATH}/*.embl; do
 	[ -e "$f" ] && ls $f >> genomes.list
 done
 
-for f in ../Data/genomes/*.fasta; do
+for f in ${$INPUT_DATA_PATH}/*.fasta; do
 	[ -e "$f" ] && ls $f >> genomes.list
 done
 
-for f in ../Data/genomes/*.fna; do
+for f in ${$INPUT_DATA_PATH}/*.fna; do
 	[ -e "$f" ] && ls $f >> genomes.list
 done
 
-for f in ../Data/genomes/*.fa; do
+for f in ${$INPUT_DATA_PATH}/*.fa; do
 	[ -e "$f" ] && ls $f >> genomes.list
 done
 
@@ -76,44 +122,44 @@ done
 perl mult_antiSMASH.pl -i genomes.list -c $NPROC
 
 # moves output antiSMASH directories to ../Data/antiSMASH_annotations/
-if [ -d "../Data/antiSMASH_annotations/" ]
+if [ -d $OUTPUT_DIR ]
 then
-	echo "../Data/antiSMASH_annotations found - moving antiSMASH output directories there"
+	echo "$OUTPUT_DIR found - moving antiSMASH output directories there"
 else
-	echo "Making ../Data/antiSMASH_annotations - moving antiSMASH directories there"
-	mkdir ../Data/antiSMASH_annotations
+	echo "Making $OUTPUT_DIR - moving antiSMASH directories there"
+	mkdir $OUTPUT_DIR
 fi
-mv */ ../Data/antiSMASH_annotations
-mv files.list ../Data/antiSMASH_annotations
+mv */ $OUTPUT_DIR
+mv files.list $OUTPUT_DIR
 
 # runs cluster_renamer.pl on newly annotated gbks
-ls ../Data/antiSMASH_annotations/*/*cluster*.gbk > BGCs.list
+ls ${OUTPUT_DIR}/*/*cluster*.gbk > BGCs.list
 perl cluster_renamer.pl -i BGCs.list -c $NPROC
 
 # moves cluster_renamer.pl output files
-echo "Moving renamed clusters to ../Data/BGC_gbks/"
-mv BGC_gbks ../Data/
-if [ -d "../Results/" ]
+echo "Moving renamed clusters to $OUTPUT_DIR/BGC_gbks/"
+mv BGC_gbks $OUTPUT_DIR
+if [ -d "${OUTPUT_DIR}/Results/" ]
 then
-	echo "../Results/ found - moving nodes.tsv there"
+	echo "${OUTPUT_DIR}/Results/ found - moving nodes.tsv there"
 else
-	echo "Making ../Results - moving nodes.tsv there"
-	mkdir ../Results
+	echo "Making ${OUTPUT_DIR}/Results - moving nodes.tsv there"
+	mkdir ${OUTPUT_DIR}/Results
 fi
-mv nodes.tsv ../Results/
+mv nodes.tsv ${OUTPUT_DIR}/Results
 
 # runs clusters_per_genome.pl on new nodes.tsv file
-perl clusters_per_genome.pl -i ../Results/nodes.tsv
+perl clusters_per_genome.pl -i ${OUTPUT_DIR}/Results/nodes.tsv
 
 # moves clusters_per_genome.pl output files
-echo "Moving clusters_per_genome.tsv to ../Results"
-mv clusters_per_genome.tsv ../Results
+echo "Moving clusters_per_genome.tsv to ${OUTPUT_DIR}/Results"
+mv clusters_per_genome.tsv ${OUTPUT_DIR}/Results
 
 # Cleans up src/
 rm genomes.list
 rm BGCs.list
 
 # Add note to CHANGELOG.txt
-echo -e "* antiSMASH_annotations.sh v3.0 finished `date +%Y-%m-%d:%H:%M:%S`\n" >> ../CHANGELOG.txt
+echo -e "* antiSMASH_annotations.sh v3.0 finished `date +%Y-%m-%d:%H:%M:%S`\n" >> $LOG_FILE
  
 
